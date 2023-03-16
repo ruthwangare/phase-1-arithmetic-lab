9abc33400bbab06f2fc96b4473a689c0a80a502b
Ruth <rwangare124@gmail.com>
2023-03-16 12:29:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random); // Output: a random integer between 1 and 100
+
+let num3 = 22;
+let num4 = 9;
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+
+let set = [5, 10, 15, 20, 7];
+let max = Math.max(...set);
+console.log(max); // Output: 20
+
+
+
